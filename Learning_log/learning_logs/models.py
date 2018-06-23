0c2ac9de8396a7c58e8c51cdc5589487392e99e8
Mrinal <waliamrinal@gmail.com>
2018-06-23 21:01:05 +0530

    add these changes into files

--- Learning_log/learning_logs/models.py
+++ Learning_log/learning_logs/models.py
@@ -12,24 +12,29 @@ class Topic(models.Model):
 	#data that will record a date and time
 	date_added = models.DateTimeField(auto_now_add = True)
 	
+	def __str__(self):
+		"""Return a string representation of the model."""
+		return self.text
+	
 class Entry(models.Model):
 	'''something specific learned about the topic'''
 	'''A foreign key is a database term; it’s a reference to another record in the database. 
 	This is the code that connects each entry to a specific topic.
 	 Each topic is assigned a key, or ID, when it’s created.
 	'''
-	topic = models.ForeignKey(Topic)
+	topic = models.ForeignKey('Topic', on_delete=models.PROTECT)
 	text = models.TextField()
-	date_added = models.DateTimeField(auto_now_add = True)
-class Meta:
-	'''Meta holds extra information for managing a model; 
-	here it allows us to set a special attribute telling Django to 
-	use Entries when it needs to refer to more than one entry. 
-	'''
-	verbose_name_plural = 'entries'
+	date_added = models.DateTimeField(auto_now_add=True)
+	
+	class Meta:
+		'''Meta holds extra information for managing a model; 
+		here it allows us to set a special attribute telling Django to 
+		use Entries when it needs to refer to more than one entry. 
+		'''
+		verbose_name_plural = 'entries'
 	
 	def __str__(self):
 		'''return a string representation of the module'''
-		return self.text[:50] + '...' 
+		return self.text[:50] + "..." 
 		
 	
