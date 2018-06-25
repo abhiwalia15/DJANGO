a660c3e315aa6c6d312688f51855f3fe65dbbfc1
Mrinal <waliamrinal@gmail.com>
2018-06-25 12:46:31 +0530

    add files

--- Learning_log/learning_log/urls.py
+++ Learning_log/learning_log/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#The first two lines import the functions and modules that manage URLs for the project and admin site 
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'', include('learning_logs.url', namespace = 'learning_logs')),
 ]
+
+
