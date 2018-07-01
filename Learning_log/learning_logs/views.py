1542836747966ec6bd6033eaecbfbd565f223a8b
Mrinal <waliamrinal@gmail.com>
2018-07-01 22:56:09 +0530

    topics template html file

--- Learning_log/learning_logs/views.py
+++ Learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 '''A view function takes in information from a request, prepares the data 
 needed to generate a page, and then sends the data back to the browser, 
@@ -7,3 +8,9 @@ often by using a template that defines what the page will look like.'''
 def index(request):
 	'''the home page for learning logs'''
 	return render(request, 'learning_logs/index.html')
+
+def topics(request):
+	'''show all topics'''
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics':topics}
+	return render(request, 'learning_logs/topics.html', context)
