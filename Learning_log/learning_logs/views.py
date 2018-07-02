5aeb085c8f50d4fcf2fc60f322fbd4256c976d64
Mrinal <waliamrinal@gmail.com>
2018-07-02 15:51:14 +0530

    changes made in views.py

--- Learning_log/learning_logs/views.py
+++ Learning_log/learning_logs/views.py
@@ -14,3 +14,14 @@ def topics(request):
 	topics = Topic.objects.order_by('date_added')
 	context = {'topics':topics}
 	return render(request, 'learning_logs/topics.html', context)
+
+'''This is the first view function that requires a parameter other than the request object
+The function accepts the value captured by the 
+expression (?P<topic_id>\d+) and stores it in topic_id'''
+
+def topic(requests, topic_id):
+	'''show a single topic and all its entries'''
+	topic = Topic.objects.get(id = topic_id)
+	#the minus sign infront of the date_added sorts the result in reverse order, which will display the most recent entries first.
+	entries = topic.entry_set.order_by('-date_added')
+	return render(request, 'learning_logs/topic.html', context)
