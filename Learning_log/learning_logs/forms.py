913bfff981cb0b02e1ca725fe8bb5bb203715eb3
Mrinal <waliamrinal@gmail.com>
2018-07-07 01:47:53 +0530

    entry form

--- Learning_log/learning_logs/forms.py
+++ Learning_log/learning_logs/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 #now we define a class called TopicForm, which inherits from forms.ModelForm.
 class TopicForm(forms.ModelForm):
@@ -9,3 +9,13 @@ class TopicForm(forms.ModelForm):
 		model = Topic
 		fields = ['text']
 		labels = {'text':''}
+
+class EntryForm(forms.ModelForm):
+	class Meta:
+		model = Entry
+		fields = ['text']
+		labels = {'text':''}
+		#A widget is an HTML form element, such as a single-line text box, multi-line text area, or drop-down list.
+		#By telling Django to use a forms.Textarea element, we’re customizing the input widget for the field 'text' 
+		#so the text area will be 80 columns wide instead of the default 40.
+		widgets = {'text':forms.Textarea(attrs={'cols':80})}
