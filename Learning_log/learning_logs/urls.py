7e0178e5548909f9e8a1e7b08b3a848b8182f431
Mrinal <waliamrinal@gmail.com>
2018-07-07 13:42:25 +0530

    new entry urls

--- Learning_log/learning_logs/urls.py
+++ Learning_log/learning_logs/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 		url(r'^new_topic/$', views.new_topic, name='new_topic'),
 		#page for adding a new entry.
 		#The code (?P<topic_id>\d+) captures a numerical value and stores it in the variable topic_id
-		url(r'^new_entry/(?p<topic_id>\d+)$', views.new_entry, name = 'new_entry')
+		url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 	]
 	
 
