7df0249699f2f7839d76675fbd1eaf4674ab2569
Mrinal <waliamrinal@gmail.com>
2018-07-08 13:13:00 +0530

    changes

--- Learning_log/learning_logs/views.py
+++ Learning_log/learning_logs/views.py
@@ -86,7 +86,7 @@ def edit_entry(request, entry_id):
 		
 		if form.is_valid():
 			form.save()
-			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
 			
 	context = {'entry':entry, 'topic':topic, 'form':form}
 	return render(request, 'learning_logs/edit_entry.html', context)
